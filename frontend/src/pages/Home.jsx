676e448193abee3f00c416482092607e4216cc87
Deepak Aggarwal <deepakagg1206@gmail.com>
2025-09-05 18:08:54 +0530

    Delete issue

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -46,7 +46,7 @@ export const Home = () => {
       await axios.delete(`${config.apiBaseUrl}/api/v1/blog/${Blogid}`, {
         withCredentials: true,
       });
-      window.location.reload();
+      navigate("/home");
     } catch (err) {
       console.error("Error occured while deleting");
     }
